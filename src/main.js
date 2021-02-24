d01a98202ec1611c5d4e216e77de77f91ea75107
adamkjall <adam.kjall@hotmail.com>
2021-02-24 13:19:28 +0100

    added tailwind to the project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
+
+import "./assets/styles/index.scss";
 
 createApp(App)
   .use(router)
